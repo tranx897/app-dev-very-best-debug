610b508414117455480318f75a340121184b5c11
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-18 00:30:14 +0000

    debugged venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -10,7 +10,7 @@ class VenuesController < ApplicationController
   def show
     venue_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    @the_venue = matching_venues.first
 
     render({ :template => "venue_templates/details" })
   end
@@ -28,9 +28,9 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
+    venue = Venue.where({ :id => the_id }).first
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
